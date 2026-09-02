4fb335f07ae4a58ff655290e23fc34bc99b4d687
GVIKAS17 <146604769+GVIKAS17@users.noreply.github.com>
2026-09-02 21:06:44 +0530

    Time: 5 ms (45.76%), Space: 13.2 MB (94.38%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = len(nums)
+        n = 0
+        m = 0
+        for i in nums:
+            n += i
+        for i in range(1,a+1):
+            m += i
+        return m - n
