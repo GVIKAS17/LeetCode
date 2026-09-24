c2a47ad9bde9b1e56f84828dc7edfbf5b8280c00
GVIKAS17 <146604769+GVIKAS17@users.noreply.github.com>
2026-09-24 06:40:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def dailyTemperatures(self, temp):
+        n = len(temp)
+        answer = [0] * n
+        stack = []
+        for i in range(n):
+            while stack and temp[i] > temp[stack[-1]]:
+                prev = stack.pop()
+                answer[prev] = i - prev
+            stack.append(i)
+        return stack
